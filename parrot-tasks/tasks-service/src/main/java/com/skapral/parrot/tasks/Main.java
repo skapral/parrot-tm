61be2f2d5dde760d060ffa4c96b69f1d8faa3bd4
Sergey Kapralov <skapralov@mail.ru>
2021-10-20 21:14:40 +0300

    tasks API security

--- parrot-tasks/tasks-service/src/main/java/com/skapral/parrot/tasks/Main.java
+++ parrot-tasks/tasks-service/src/main/java/com/skapral/parrot/tasks/Main.java
@@ -3,12 +3,13 @@ package com.skapral.parrot.tasks;
 
 import com.skapral.parrot.tasks.data.SpringDataJdbc;
 import com.skapral.parrot.tasks.events.Amqp;
+import com.skapral.parrot.tasks.security.SecurityConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({Amqp.class, SpringDataJdbc.class})
+@Import({Amqp.class, SpringDataJdbc.class, SecurityConfig.class})
 public class Main {
     public static void main(String... args) {
         SpringApplication.run(Main.class);
---
+++ parrot-tasks/tasks-service/src/main/java/com/skapral/parrot/tasks/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.skapral.parrot.tasks.security;
+
+import com.skapral.parrot.auth.common.jwt.JwtAuthFilter;
+import com.skapral.parrot.auth.common.security.JwtConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+@Configuration
+@Import(JwtConfig.class)
+@EnableWebSecurity
+public class SecurityConfig extends com.skapral.parrot.auth.common.security.SecurityConfig {
+    @Autowired
+    public SecurityConfig(AuthenticationEntryPoint authEntryPoint, JwtAuthFilter authFilter) {
+        super(authEntryPoint, authFilter);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
+}
