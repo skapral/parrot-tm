8343f0ef6ba591e86bf7d2dd3d1d9fbf3c5af03d
Sergey Kapralov <skapralov@mail.ru>
2021-10-31 00:55:21 +0300

    restructure

--- parrot-accounting/accounting-service/src/main/java/com/skapral/parrot/accounting/Main.java
+++ parrot-accounting/accounting-service/src/main/java/com/skapral/parrot/accounting/Main.java
@@ -3,10 +3,8 @@ package com.skapral.parrot.accounting;
 import com.skapral.parrot.accounting.events.EventsInbox;
 import com.skapral.parrot.accounting.security.SecurityConfig;
 import com.skapral.parrot.common.data.SpringDataJdbc;
-import com.skapral.parrot.common.events.EventsConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
